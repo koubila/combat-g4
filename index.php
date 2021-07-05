69467aadfb410192d2c8c409cd588f456718dccd
jperaudon <j.peraudon@it-akademy.fr>
2021-07-05 12:22:11 +0000

    exo

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
 require 'classes/Voiture.php';
 
-$voiture1 = new Voiture;
-$voiture2 = new Voiture;
+$voiture1 = new Voiture('vert', 4); // new = instancie la classe
+$voiture2 = new Voiture('blanc', 7);
+
+$voiture1->avance(85);
+echo '<br>';
+
 
 var_dump($voiture1, $voiture2);
